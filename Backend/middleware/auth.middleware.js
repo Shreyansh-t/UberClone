085a8435bc36d06b9ed5804a488865f3b98fe3e3
Shreyansh-t <96645080+Shreyansh-t@users.noreply.github.com>
2025-06-17 17:54:38 +0530

    added react ui

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -1,19 +1,20 @@
 const userModel = require("../models/user.model");
+const captainModel = require("../models/captain.model.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const blackListTokenModel = require("../models/blacklistToken.model");
 
-module.exports.authUser = async(req, resizeBy, next)=>{
+module.exports.authUser = async(req, res, next)=>{
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
 
     if(!token){
-        return resizeBy.status(401).json({message: "Unauthorized"});
+        return res.status(401).json({message: "Unauthorized"});
     }
 
     const isBlackListed = await blackListTokenModel.findOne({token: token});
 
     if(isBlackListed){
-        return resizeBy.status(401).json({message: 'Unauthorized'});
+        return res.status(401).json({message: 'Unauthorized'});
     }
 
     try{
@@ -25,6 +26,32 @@ module.exports.authUser = async(req, resizeBy, next)=>{
         return next();
     }
     catch (err){
-        return resizeBy.status(401).json({message: "Unauthorized"});
+        return res.status(401).json({message: "Unauthorized"});
+    }
+}
+
+module.exports.authCaptain = async(req, res, next)=>{
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    if(!token){
+        return res.status(401).json({message: "Unauthorized"});
+    }
+
+    const isBlackListed = await blackListTokenModel.findOne({token: token});
+
+    if(isBlackListed){
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded.id);
+
+        req.captain = captain;
+
+        return next();
+    }
+    catch (err){
+        return res.status(401).json({message: "Unauthorized"});
     }
 }
